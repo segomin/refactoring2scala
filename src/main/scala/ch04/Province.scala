f40fea8f96c74c68158cd3f4fe9879474d285ca7
sangho <segoviamin@gmail.com>
2025-02-16 11:36:34 +0900

    ✨(ch04-3): 첫 번째 테스트

--- src/main/scala/ch04/Province.scala
+++ src/main/scala/ch04/Province.scala
@@ -4,8 +4,7 @@ package ch04
 /**
  * 생산 계획은 각 지역(Province)의 수요(demand)와 가격(price)으로 구정
  * 생산자들은 각기 제품을 특정 가격으로 특정 수량만큼 생산
- * 생산ㅈ자 별로 제품을 모두 판매시 수익: full revenue
- *
+ * 생산자 별로 제품을 모두 판매시 수익: full revenue
  */
 
 /**
@@ -13,15 +12,17 @@ package ch04
  */
 class Province(
                 val name: String,
-                _producers: List[Producer],
-                var totalProduction: Int = 0,
                 var demand: Int,
                 var price: Int,
               ) {
-  var producers: List[Producer] = _producers.map(p => {
-    p.province = (this)
-    p
-  })
+  var totalProduction: Int = 0
+  var producers: List[Producer] = List()
+
+  def addProducer(producer: Producer) = {
+    producer.province = this
+    producers = producer :: producers
+    totalProduction += producer.production
+  }
 
   def shortfall: Int = demand - totalProduction
 
@@ -56,19 +57,4 @@ class Producer(val name: String, val cost: Int, var production: Int = 0) {
     province.totalProduction += newProduction - production
     production = newProduction
   }
-}
-
-def sampleProvinceData: Province = {
-  val producers = List(
-    Producer("Byzantium", 10, 9),
-    Producer("Attalia", 12, 10),
-    Producer("Sinope", 10, 6),
-  )
-  Province("Asia", producers, 30, 21, 10)
-}
-
-@main def ch04(): Unit = {
-  val asia = sampleProvinceData
-
-  println(asia.shortfall)
 }
---
+++ src/test/scala/ch04/ProvinceTest.scala
@@ -0,0 +1,21 @@
+package org.sangho.refac2scala
+package ch04
+
+class ProvinceTest extends munit.FunSuite:
+  def sampleProvinceData: Province = {
+    val producers = List(
+      Producer("Byzantium", 10, 9),
+      Producer("Attalia", 12, 10),
+      Producer("Sinope", 10, 6),
+    )
+    val asia = Province("Asia", 30, 20)
+    for (producer <- producers) {
+      asia.addProducer(producer)
+    }
+    asia
+  }
+
+  test("shortfall") {
+    val asia = sampleProvinceData
+    assertEquals(asia.shortfall, 5)
+  }
