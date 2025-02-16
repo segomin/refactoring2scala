f0b7cdb1beb9537247a074846cdb7e41ff068d75
sangho <segoviamin@gmail.com>
2025-02-16 13:19:18 +0900

    ✨(ch04-4): setup 픽스처

--- src/test/scala/ch04/ProvinceTest.scala
+++ src/test/scala/ch04/ProvinceTest.scala
@@ -1,26 +1,32 @@
 package org.sangho.refac2scala
 package ch04
 
+import scala.compiletime.uninitialized
+
 class ProvinceTest extends munit.FunSuite:
-  def sampleProvinceData: Province = {
-    val producers = List(
-      Producer("Byzantium", 10, 9),
-      Producer("Attalia", 12, 10),
-      Producer("Sinope", 10, 6),
-    )
-    val asia = Province("Asia", 30, 20)
-    for (producer <- producers) {
-      asia.addProducer(producer)
-    }
-    asia
+  var asia: Province = uninitialized
+
+  override def beforeAll(): Unit = {
+    asia = sampleProvinceData
   }
 
   test("shortfall") {
-    val asia = sampleProvinceData
     assertEquals(asia.shortfall, 5)
   }
 
   test("profit") {
-    val asia = sampleProvinceData
     assertEquals(asia.profit, 230)
   }
+
+def sampleProvinceData: Province = {
+  val producers = List(
+    Producer("Byzantium", 10, 9),
+    Producer("Attalia", 12, 10),
+    Producer("Sinope", 10, 6),
+  )
+  val asia = Province("Asia", 30, 20)
+  for (producer <- producers) {
+    asia.addProducer(producer)
+  }
+  asia
+}
