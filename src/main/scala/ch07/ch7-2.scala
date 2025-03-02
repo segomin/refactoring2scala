3f61fd9225bd45e43f7ea9e7af07425db1e8387f
sangho <segoviamin@gmail.com>
2025-03-02 22:37:52 +0900

    ♻️️(ch07): 7-2 컬렉션 캡슐화하기 : 컬렉션 수정 캡슐화 (add)

--- src/main/scala/ch07/ch7-2.scala
+++ src/main/scala/ch07/ch7-2.scala
@@ -3,18 +3,25 @@ package ch07_2
 
 import scala.collection.mutable
 
-class Course (val name: String, val isAdvanced: Boolean)
+case class Course (name: String, isAdvanced: Boolean)
 
 class Person (val name: String) {
-  private var _courses: mutable.Set[Course] = mutable.Set()
+  private val _courses: mutable.Set[Course] = mutable.Set()
 
   def courses: mutable.Set[Course] = _courses
-  def courses_=(list: mutable.Set[Course]): Unit = _courses = list
+
+  def addCourse(course: Course): Unit = _courses.add(course)
+  def removeCourse(course: Course): Unit = _courses.remove(course)
 }
 
 @main def main(): Unit = {
   val kent = new Person("마틴")
-  kent.courses = mutable.Set(new Course("리펙토링", false), new Course("리펙토링2nd", true))
+  kent.addCourse(Course("리펙토링", false))
+  kent.addCourse(Course("리펙토링2nd", true))
   val numAdvancedCourses = kent.courses.count(course => course.isAdvanced)
   assert(numAdvancedCourses == 1)
+
+  kent.removeCourse(Course("리펙토링2nd", true))
+  assert(kent.courses.size == 1)
+  assert(kent.courses.count(course => course.isAdvanced) == 0)
 }
