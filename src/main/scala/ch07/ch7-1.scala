1e07d256e216ee5e54a81c34ed02c8c587774070
sangho <segoviamin@gmail.com>
2025-03-02 21:33:41 +0900

    ♻️️(ch07): 7-1 변수 캡슐화하기 (클래스로 변경 - 중간 함수 단계 생략)

--- src/main/scala/ch07/ch7-1.scala
+++ src/main/scala/ch07/ch7-1.scala
@@ -5,9 +5,27 @@ import scala.collection.mutable
 
 val organization = mutable.Map("name" -> "애크미 구스베리", "country" -> "GB")
 
+class Order(data: mutable.Map[String, String]) {
+  private var _data = data
+
+  def name = _data("name")
+
+  def country = _data("country")
+
+  def name_=(newName: String): Unit = {
+    _data("name") = newName
+  }
+
+  def country_=(newCountry: String): Unit = {
+    _data("country") = newCountry
+  }
+}
+
+// main
 @main def main(): Unit = {
-  val result = s"""<h1>${organization("name")}</h1><p>${organization("country")}</p>"""
+  val order = new Order(organization)
+  val result = s"""<h1>${order.name}</h1><p>${order.country}</p>"""
   assert(result == "<h1>애크미 구스베리</h1><p>GB</p>")
-  organization("name") = "사과"
-  assert(organization("name") == "사과")
+  order.name = "사과"
+  assert(order.name == "사과")
 }
