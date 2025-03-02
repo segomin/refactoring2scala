3450c66779aaa79cc44be41f6da31cc0ed646433
sangho <segoviamin@gmail.com>
2025-03-02 22:15:17 +0900

    ♻️️(ch07): 7-1 중첩된 레코드 클래스 캡슐화 (중간함수 생략)

--- src/main/scala/ch07/ch7-1-2.scala
+++ src/main/scala/ch07/ch7-1-2.scala
@@ -3,17 +3,30 @@ package ch07_1_2
 
 import scala.collection.mutable
 
-val nestedRecord = mutable.Map(
+val nestedRecord: mutable.Map[String, mutable.Map[String, Object]] = mutable.Map(
   "1920" -> mutable.Map("name" -> "마틴 파울러", "id" -> "1920",
     "usages" -> mutable.Map(
       "2016" -> mutable.Map("1" -> 50, "2" -> 55),
       "2015" -> mutable.Map("1" -> 70, "2" -> 63)))
 )
 
+class CustomData (data: mutable.Map[String, mutable.Map[String, Object]]) {
+  private val _data = data
+
+  def getUsage(customerID: String, year: String, month: String): Int = {
+    _data(customerID)("usages").asInstanceOf[mutable.Map[String, mutable.Map[String, Int]]](year)(month)
+  }
+
+  def setUsage(customerID: String, year: String, month: String, amount: Int): Unit = {
+    _data(customerID)("usages").asInstanceOf[mutable.Map[String, mutable.Map[String, Int]]](year)(month) = amount
+  }
+}
+
 @main def main(): Unit = {
   // 쓰기예
-  nestedRecord("1920")("usages").asInstanceOf[mutable.Map[String, mutable.Map[String, Int]]]("2015")("1") = 80
-  val result =nestedRecord("1920")("usages").asInstanceOf[mutable.Map[String, mutable.Map[String, Int]]]("2015")("1")
+  val customData = new CustomData(nestedRecord)
+  customData.setUsage("1920", "2015", "1", 80)
+  val result = customData.getUsage("1920", "2015", "1")
   assert(result == 80)
 
   // 읽기예
