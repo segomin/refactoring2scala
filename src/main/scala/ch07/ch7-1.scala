3c3b0b76dae671de1918170f00478fdfe85e6043
sangho <segoviamin@gmail.com>
2025-03-02 21:24:51 +0900

    ✨️(ch07): 7-1 init 간단한 레코드 캡슐화하기

--- src/main/scala/ch07/ch7-1.scala
+++ src/main/scala/ch07/ch7-1.scala
@@ -1,32 +1,13 @@
 package org.sangho.refac2scala
 package ch07_1
 
-val organization = Map("name" -> "애크미 구스베리", "country" -> "GB")
+import scala.collection.mutable
 
-class Order(data: Map[String, String]) {
-  private var _name = data("name")
-  private var _country = data("country")
-
-  def name = _name
-
-  def country = _country
-
-  def name_=(newName: String): Unit = {
-    _name = newName
-  }
-
-  def country_=(newCountry: String): Unit = {
-    _country = newCountry
-  }
-}
+val organization = mutable.Map("name" -> "애크미 구스베리", "country" -> "GB")
 
 @main def main(): Unit = {
-  val order = new Order(organization)
-  assert(order.name == "애크미 구스베리")
-  assert(order.country == "GB")
-
-  order.name = "사과"
-  order.country = "KO"
-  assert(order.name == "사과")
-  assert(order.country == "KO")
+  val result = s"""<h1>${organization("name")}</h1><p>${organization("country")}</p>"""
+  assert(result == "<h1>애크미 구스베리</h1><p>GB</p>")
+  organization("name") = "사과"
+  assert(organization("name") == "사과")
 }
