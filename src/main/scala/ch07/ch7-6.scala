15fb476398653137c36328a2d4e20cfbca36ea34
sangho <segoviamin@gmail.com>
2025-03-03 09:50:11 +0900

    ✨(ch07): 7-6 클래스 인라인하기 init

--- src/main/scala/ch07/ch7-6.scala
+++ src/main/scala/ch07/ch7-6.scala
@@ -1,22 +1,30 @@
 package org.sangho.refac2scala
 package ch07
 
-class Person(val name: String, _officeAreaCode: String, _officeNumber: String) {
-  private val _telephoneNumber = TelephoneNumber(_officeAreaCode, _officeNumber)
+class TrackingInformation (__shippingCompany: String, __trackingNumber: String) {
+  private var _shippingCompany = __shippingCompany
+  private var _trackingNumber = __trackingNumber
 
-  def officeAreaCode: String = _telephoneNumber.areaCode
+  def shippingCompany: String = _shippingCompany
+  def trackingNumber: String = _trackingNumber
 
-  def officeNumber: String = _telephoneNumber.number
+  def shippingCompany_=(arg: String): Unit = _shippingCompany = arg
+  def trackingNumber_=(arg: String): Unit = _trackingNumber = arg
+
+  def display: String = s"${shippingCompany}: ${trackingNumber}"
 }
 
-case class TelephoneNumber(areaCode: String, number: String) {
+class Shipment(private var _trackingInformation: TrackingInformation) {
+  def trackingInfo: String = _trackingInformation.display
+  def trackingInformation: TrackingInformation = _trackingInformation
+  def trackingInformation_=(arg: TrackingInformation): Unit = _trackingInformation = arg
 }
 
 
 @main def main(): Unit = {
-  // getter
-  val person = new Person("마틴", "010", "1234")
-  assert(person.name == "마틴")
-  assert(person.officeAreaCode == "010")
-  assert(person.officeNumber == "1234")
+  // test
+  val trackingInformation = new TrackingInformation("우체국", "1234")
+  val shipment = new Shipment(trackingInformation)
+  assert(shipment.trackingInfo == "우체국: 1234")
+  assert(shipment.trackingInformation == trackingInformation)
 }
