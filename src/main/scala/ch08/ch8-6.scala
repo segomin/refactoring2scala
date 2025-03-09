7866b4771a14e2bd617fb81fa09c2ac4fe1d6747
sangho <segoviamin@gmail.com>
2025-03-09 21:35:30 +0900

    ♻️(ch08): 8-6 문장을 슬라이드 하기 : 함수 이동

--- src/main/scala/ch08/ch8-6.scala
+++ src/main/scala/ch08/ch8-6.scala
@@ -3,7 +3,18 @@ package ch08_6
 
 case class Order(units: Int, isRepeat: Boolean)
 
-case class PricingPlan(base: Double, unit: Double, discountThreshold: Int, discountFactor: Double)
+case class PricingPlan(base: Double, unit: Double, discountThreshold: Int, discountFactor: Double) {
+  def calculateCharge(order: Order, pricingPlan: PricingPlan) = {
+    val chargePerUnit = pricingPlan.unit
+    val units = order.units
+    val charge = pricingPlan.base + units * chargePerUnit
+    val discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0)
+    var discount = discountableUnits * pricingPlan.discountFactor
+    if (order.isRepeat) discount += 20
+    val finalCharge = charge - discount
+    finalCharge
+  }
+}
 
 def retrievePricingPlan(): PricingPlan = {
   PricingPlan(base = 10, unit = 1, discountThreshold = 100, discountFactor = 0.1)
@@ -15,21 +26,11 @@ def chargeOrder(charge: Double): String = {
   s"Charge: $charge"
 }
 
-def calculateCharge(order: Order, pricingPlan: PricingPlan) = {
-  val chargePerUnit = pricingPlan.unit
-  val units = order.units
-  val charge = pricingPlan.base + units * chargePerUnit
-  val discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0)
-  var discount = discountableUnits * pricingPlan.discountFactor
-  if (order.isRepeat) discount += 20
-  val finalCharge = charge - discount
-  finalCharge
-}
 
 @main def main(): Unit = {
   val order = retrieveOrder()
   val pricingPlan = retrievePricingPlan()
-  val finalCharge = calculateCharge(order, pricingPlan)
+  val finalCharge = pricingPlan.calculateCharge(order, pricingPlan)
   val actual = chargeOrder(finalCharge)
 
   assert(actual == "Charge: 90.0")
