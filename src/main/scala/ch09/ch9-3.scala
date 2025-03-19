740c7262acdb236171955200af051ee82ac07e47
sangho <segoviamin@gmail.com>
2025-03-19 19:33:46 +0900

    ♻️️(ch09): 9.3 파생 변수를 질의 함수로 바꾸기 : check

--- src/main/scala/ch09/ch9-3.scala
+++ src/main/scala/ch09/ch9-3.scala
@@ -7,7 +7,14 @@ class ProductionPlan {
   private var _production = 0
   private var _adjustments = List[Adjustment]()
 
-  def production: Int = _production
+  def production: Int = {
+    assert(_production == calculatedProduction)
+    _production
+  }
+
+  private def calculatedProduction: Int = {
+    _adjustments.foldLeft(0)((sum, a) => sum + a.amount)
+  }
 
   def applyAdjustment(anAdjustment: Adjustment): Unit = {
     _adjustments = anAdjustment :: _adjustments
