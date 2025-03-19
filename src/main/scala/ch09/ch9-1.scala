d623feed3ca2f5b5839ed5aee3cbae6c659770e7
sangho <segoviamin@gmail.com>
2025-03-19 18:59:49 +0900

    ♻️(ch09): 9.1 변수 쪼개기 : 추가
    todo: 함수를 객체로 만들어 보기

--- src/main/scala/ch09/ch9-1.scala
+++ src/main/scala/ch09/ch9-1.scala
@@ -4,27 +4,27 @@ package ch09_1
 case class Scenario(primaryForce: Int, secondaryForce: Int, mass: Int, delay: Int)
 
 def distanceTravelled(scenario: Scenario, time: Int): Int = {
-  var result: Int = 0
   // 가속도(a) = 힘(F) / 질량(m)
   val primaryAcceleration = scenario.primaryForce / scenario.mass
   val primaryTime = Math.min(time, scenario.delay)
   // 전파된 거리
-  result = (0.5 * primaryAcceleration * primaryTime * primaryTime).toInt
+  val primaryDistance: Int = (0.5 * primaryAcceleration * primaryTime * primaryTime).toInt
   val secondaryTime = time - scenario.delay
-  if (secondaryTime > 0) {
-    // 두 번째 힘을 반영해 다시 계산
-    val primaryVelocity = primaryAcceleration * scenario.delay
-    val secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass
-    result += primaryVelocity * secondaryTime + (0.5 * secondaryAcceleration * secondaryTime * secondaryTime).toInt
+  if (secondaryTime <= 0) {
+    return primaryDistance
   }
-  result
+
+  // 두 번째 힘을 반영해 다시 계산
+  val primaryVelocity = primaryAcceleration * scenario.delay
+  val secondaryAcceleration = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass
+  val secondaryDistance = primaryVelocity * secondaryTime + (0.5 * secondaryAcceleration * secondaryTime * secondaryTime).toInt
+  primaryDistance + secondaryDistance
 }
 
 
 // main
 @main def main(): Unit = {
-  val scenario = Scenario(10, 20, 30, 40)
-  val actual = distanceTravelled(scenario, 100)
-  println(actual)
-  assert(actual == 1800)
+  val scenario = Scenario(30, 20, 10, 40)
+  assert(distanceTravelled(scenario, 35) == 1837)
+  assert(distanceTravelled(scenario, 100) == 18600)
 }
