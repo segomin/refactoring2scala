abde9e18b36a94566a9c1e8200451f1726941e1a
sangho <segoviamin@gmail.com>
2025-03-19 19:35:08 +0900

    ♻️️(ch09): 9.3 파생 변수를 질의 함수로 바꾸기 : 미사용 삭제

--- src/main/scala/ch09/ch9-3.scala
+++ src/main/scala/ch09/ch9-3.scala
@@ -4,7 +4,6 @@ package ch09_3
 case class Adjustment(amount: Int)
 
 class ProductionPlan {
-  private var _production = 0
   private var _adjustments = List[Adjustment]()
 
   def production: Int = {
@@ -13,7 +12,6 @@ class ProductionPlan {
 
   def applyAdjustment(anAdjustment: Adjustment): Unit = {
     _adjustments = anAdjustment :: _adjustments
-    _production += anAdjustment.amount
   }
 }
 
