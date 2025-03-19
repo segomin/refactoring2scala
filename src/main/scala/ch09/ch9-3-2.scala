6a9ca429ae244c0c9c4a86c2fd52e32b8b6b667a
sangho <segoviamin@gmail.com>
2025-03-19 19:43:42 +0900

    ♻️️️(ch09): 9.4 파생 변수를 질의 함수로 바꾸기 - 소스가 둘 이상일 때 : 어셔선 추가(확인)

--- src/main/scala/ch09/ch9-3-2.scala
+++ src/main/scala/ch09/ch9-3-2.scala
@@ -4,21 +4,29 @@ package ch09_3_2
 case class Adjustment(amount: Int)
 
 class ProductionPlan (__production: Int) {
-  private var _production = __production
+  private var _initialProduction = __production
+  private var _productionAccumulator = 0
   private var _adjustments = List[Adjustment]()
 
-  def production: Int = _production
+  def production: Int = {
+    assert(_productionAccumulator == calculateProductionAccumulator)
+    _initialProduction + _productionAccumulator
+  }
+
+  def calculateProductionAccumulator: Int = {
+    _adjustments.foldLeft(0)((sum, a) => sum + a.amount)
+  }
 
   def applyAdjustment(anAdjustment: Adjustment): Unit = {
     _adjustments = anAdjustment :: _adjustments
-    _production += anAdjustment.amount
+    _productionAccumulator += anAdjustment.amount
   }
 }
 
 // main
 @main def main(): Unit = {
-  val plan = new ProductionPlan(0)
+  val plan = new ProductionPlan(10)
   plan.applyAdjustment(Adjustment(10))
   plan.applyAdjustment(Adjustment(20))
-  assert(plan.production == 30)
+  assert(plan.production == 40)
 }
