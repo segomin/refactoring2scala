8976d992b48fe1f819c998a1528c354ede315fef
sangho <segoviamin@gmail.com>
2025-03-22 11:11:43 +0900

    ✨️️️️(ch10): 10.1 조건문 분해하기

---
+++ src/main/scala/ch10/ch10-1.scala
@@ -0,0 +1,26 @@
+package org.sangho.refac2scala
+package ch10_1
+
+case class Date(timestamp: Long) {
+  def isBefore(other: Date): Boolean = timestamp < other.timestamp
+
+  def isAfter(other: Date): Boolean = timestamp > other.timestamp
+}
+
+case class Plan(summerStart: Date, summerEnd: Date, summerRate: Int, regularRate: Int, regularServiceCharge: Int)
+
+def chargeOf(date: Date, plan: Plan, quantity: Int): Int = {
+  var charge: Int = 0
+  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
+    charge = quantity * plan.summerRate
+  else
+    charge = quantity * plan.regularRate + plan.regularServiceCharge
+  charge
+}
+
+// main
+@main def main(): Unit = {
+  val plan = Plan(Date(100), Date(200), 10, 20, 30)
+  assert(chargeOf(Date(150), plan, 10) == 100)
+  assert(chargeOf(Date(250), plan, 10) == 230)
+}
