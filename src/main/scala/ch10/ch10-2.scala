6a74106fc1191fe76529f1583db0adb6207b5c84
sangho <segoviamin@gmail.com>
2025-03-22 11:28:56 +0900

    ♻️(ch10): 10.2 조건식 통합하기 : or 사용

--- src/main/scala/ch10/ch10-2.scala
+++ src/main/scala/ch10/ch10-2.scala
@@ -4,9 +4,9 @@ package ch10_2
 case class Employee(seniority: Int, monthsDisabled: Int, isPartTime: Boolean)
 
 def disabilityAmount(employee: Employee): Int = {
-  if (employee.seniority < 2) return 0
-  if (employee.monthsDisabled > 12) return 0
-  if (employee.isPartTime) return 0
+  if (employee.seniority < 2
+    || employee.monthsDisabled > 12
+    || employee.isPartTime) return 0
 
   // 장애 수당 계산
   employee.seniority + 100
