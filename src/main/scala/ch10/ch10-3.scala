7c82d9398adc92d35b2c691513ba5b777f674fe8
sangho <segoviamin@gmail.com>
2025-03-22 11:50:31 +0900

    ✨️(ch10): 10.3 중첩 조건문을 보호 구문으로 바꾸기

---
+++ src/main/scala/ch10/ch10-3.scala
@@ -0,0 +1,51 @@
+package org.sangho.refac2scala
+package ch10_3
+
+case class Amount(amount: Int, reasonCode: String)
+
+case class Employee(isSeparated: Boolean, isRetired: Boolean, salary: Int)
+
+def payAmount(employee: Employee): Amount = {
+  var result: Amount = null
+
+  def someFinalComputation(employee: Employee) = {
+    Amount(employee.salary, "NOR")
+  }
+
+  if (employee.isSeparated) { // 퇴직한 직원인가?
+    result = Amount(0, "SEP")
+  } else {
+    if (employee.isRetired) { // 은퇴한 직원인가?
+      result = Amount(0, "RET")
+    } else {
+      // 급여 계산 로직
+      result = someFinalComputation(employee)
+    }
+  }
+  result
+}
+
+case class Instrument(capital: Int, income: Int, duration: Int, adjustmentFactor: Int, interestRate: Int)
+
+def adjustedCapital(instrument: Instrument): Int = {
+  var result = 0
+  if (instrument.capital > 0) {
+    if (instrument.interestRate > 0 && instrument.duration > 0) {
+      result = (instrument.income / instrument.duration) * instrument.adjustmentFactor
+    }
+  }
+  result
+}
+
+// main
+@main def main(): Unit = {
+  assert(payAmount(Employee(true, false, 1000)) == Amount(0, "SEP"))
+  assert(payAmount(Employee(false, true, 1000)) == Amount(0, "RET"))
+  assert(payAmount(Employee(false, false, 1000)) == Amount(1000, "NOR"))
+
+  // 조건 반대로 만들기
+  assert(adjustedCapital(Instrument(1, 100, 10, 2, 5)) == 20)
+  assert(adjustedCapital(Instrument(1, 100, 10, 2, 0)) == 0)
+  assert(adjustedCapital(Instrument(1, 100, 0, 2, 5)) == 0)
+  assert(adjustedCapital(Instrument(0, 100, 10, 2, 5)) == 0)
+}
