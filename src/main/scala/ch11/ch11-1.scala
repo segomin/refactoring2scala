0b54f0692363972aa50d7a08dd1a299204d8b138
sangho <segoviamin@gmail.com>
2025-03-29 10:15:48 +0900

    ♻️(ch11-1): 함수 복제 -> 질의함수로 변경

--- src/main/scala/ch11/ch11-1.scala
+++ src/main/scala/ch11/ch11-1.scala
@@ -19,6 +19,18 @@ def alertForMiscreant(people: List[String], alarmable: Alarmable): String = {
   ""
 }
 
+def findMiscreant(people: List[String]): String = {
+  for (p <- people) {
+    if (p == "조커") {
+      return "조커"
+    }
+    if (p == "사루만") {
+      return "사루만"
+    }
+  }
+  ""
+}
+
 // main
 @main def main(): Unit = {
   class MockAlarmable extends Alarmable {
@@ -30,13 +42,15 @@ def alertForMiscreant(people: List[String], alarmable: Alarmable): String = {
 
   val people1 = List("조커", "사루만", "배트맨")
   val mockAlarm1 = new MockAlarmable
-  val alert1 = alertForMiscreant(people1, mockAlarm1)
+  val alert1 = findMiscreant(people1)
+  alertForMiscreant(people1, mockAlarm1)
   assert(alert1 == "조커")
   assert(mockAlarm1.hasAlert)
 
   val people2 = List("배트맨", "슈퍼맨", "알라딘")
   val mockAlarm2 = new MockAlarmable
-  val alert2 = alertForMiscreant(people2, mockAlarm2)
+  val alert2 = findMiscreant(people2)
+  alertForMiscreant(people2, mockAlarm2)
   assert(alert2 == "")
   assert(!mockAlarm2.hasAlert)
 }
