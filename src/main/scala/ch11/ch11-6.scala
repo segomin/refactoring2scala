c05f008a39756cd98f62e54df1d36e9e82c22d6e
sangho <segoviamin@gmail.com>
2025-03-30 10:27:04 +0900

    ♻️(ch11-6): 추가 변경 (thermostat 꺼내기)

--- src/main/scala/ch11/ch11-6.scala
+++ src/main/scala/ch11/ch11-6.scala
@@ -12,7 +12,7 @@ class Thermostat(var _currentTemperature: Double = 0.0, var _selectedTemperature
   def selectedTemperature: Double = _selectedTemperature
 }
 
-class HeatingPlan(val _min: Double, val _max: Double, val thermostat: Thermostat) {
+class HeatingPlan(val _min: Double, val _max: Double) {
 
   def targetTemperature(selectedTemperature: Double) = {
     if (selectedTemperature > this._max) this._max
@@ -23,8 +23,7 @@ class HeatingPlan(val _min: Double, val _max: Double, val thermostat: Thermostat
 
 class Room {
   var _heatState: String = "off"
-  def setTemperature(thePlan: HeatingPlan): Unit = {
-    val thermostat = thePlan.thermostat
+  def setTemperature(thePlan: HeatingPlan, thermostat: Thermostat): Unit = {
     if (thePlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) {
       setToHeat()
     } else if (thePlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) {
@@ -49,14 +48,14 @@ class Room {
 // main
 @main def main(): Unit = {
   val room = new Room()
-  room.setTemperature(HeatingPlan(10, 20, Thermostat(15, 23)))
+  room.setTemperature(HeatingPlan(10, 20), Thermostat(15, 23))
   assert(room.heatState == "heat")
-  room.setTemperature(HeatingPlan(10, 20, Thermostat(23, 15)))
+  room.setTemperature(HeatingPlan(10, 20), Thermostat(23, 15))
   assert(room.heatState == "cool")
-  room.setTemperature(HeatingPlan(10, 20, Thermostat(10, 9)))
+  room.setTemperature(HeatingPlan(10, 20), Thermostat(10, 9))
   assert(room.heatState == "off")
-  room.setTemperature(HeatingPlan(10, 20, Thermostat(15, 15)))
+  room.setTemperature(HeatingPlan(10, 20), Thermostat(15, 15))
   assert(room.heatState == "off")
-  room.setTemperature(HeatingPlan(10, 20, Thermostat(20, 25)))
+  room.setTemperature(HeatingPlan(10, 20), Thermostat(20, 25))
   assert(room.heatState == "off")
 }
