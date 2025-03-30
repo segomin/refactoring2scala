10e048714b8983a97718d0a28b81440d66d5ff26
sangho <segoviamin@gmail.com>
2025-03-30 21:50:05 +0900

    ✨️(ch11-13): 예외를 사전확인으로 바꾸기

---
+++ src/main/scala/ch11/ch11-13.scala
@@ -0,0 +1,39 @@
+package org.sangho.refac2scala
+package ch11_13
+
+import org.scalatest.Assertions._
+
+case class Resource(id: String)
+
+class ResourcePool(init: Int = 3) {
+  private val available = scala.collection.mutable.Queue[Resource]()
+  private val allocated = scala.collection.mutable.ListBuffer[Resource]()
+
+  for (i <- 0 until init) {
+    available.enqueue(Resource(s"Resource-$i"))
+  }
+
+  def get() : Resource = {
+    var result: Resource = null
+    try {
+      result = available.dequeue()
+      allocated.append(result)
+    } catch {
+      case e: NoSuchElementException =>
+        result = Resource(s"Additional-${allocated.size + 1}")
+        allocated.append(result)
+    }
+    result
+  }
+}
+
+// main
+@main def main() = {
+  val pool = new ResourcePool(2)
+  val r1 = pool.get()
+  val r2 = pool.get()
+  val r3 = pool.get()
+  assert(r1.id == "Resource-0")
+  assert(r2.id == "Resource-1")
+  assert(r3.id == "Additional-3")
+}
