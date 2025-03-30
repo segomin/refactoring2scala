f9d7aa14de3a90d88a9f0251ff44bfc0d91be6c4
sangho <segoviamin@gmail.com>
2025-03-30 19:45:34 +0900

    ♻️(ch11-9): 함수로 추출

--- src/main/scala/ch11/ch11-9.scala
+++ src/main/scala/ch11/ch11-9.scala
@@ -20,18 +20,37 @@ class Score (candidate: Candidate, medicalExam: MedicalExam, scoringGuide: Scori
   private var highMedicalRiskFlag = false
 
   def execute(): Int = {
+    initExecute()
+    scoreSmoking()
+    scoreCertification()
+    // 비슷한 코드가 한참 이어짐
+    scoreSubByHealth()
+    result
+  }
+
+  private def initExecute(): Unit = {
+    result = 0
+    healthLevel = 0
+    highMedicalRiskFlag = false
+  }
+
+  private def scoreSmoking(): Unit = {
     if (medicalExam.isSmoker) {
       healthLevel += 10
       highMedicalRiskFlag = true
     }
+  }
+
+  private def scoreCertification(): Unit = {
     var certificationGrade = "regular"
     if (scoringGuide.stateWithLowCertification(candidate.originState)) {
       certificationGrade = "low"
       result -= 5
     }
-    // 비슷한 코드가 한참 이어짐
+  }
+
+  private def scoreSubByHealth(): Unit = {
     result -= Math.max(healthLevel - 5, 0)
-    result
   }
 }
 
