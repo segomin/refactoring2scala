f1fa06e22e77a293fa2f8bc69f5fd50186f348eb
sangho <segoviamin@gmail.com>
2025-03-30 11:50:13 +0900

    ♻️️(ch11-7): 불변 id setter 제거

--- src/main/scala/ch11/ch11-7.scala
+++ src/main/scala/ch11/ch11-7.scala
@@ -1,22 +1,17 @@
 package org.sangho.refac2scala
 package ch11_7
 
-class Person {
+class Person (val id: String){
   private var _name = ""
-  private var _id = ""
 
   def name: String = _name
   def name_=(arg: String): Unit = _name = arg
-
-  def id: String = _id
-  def id_=(arg: String): Unit = _id = arg
 }
 
 // main
 @main def main(): Unit = {
-  val martin = new Person()
+  val martin = new Person("1234")
   martin.name = "Martin"
-  martin.id = "1234"
 
   assert(martin.name == "Martin")
   assert(martin.id == "1234")
