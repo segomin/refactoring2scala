ae14e0710f30ef7d8aeaf722939715ff7a63de8d
sangho <segoviamin@gmail.com>
2025-04-06 11:06:04 +0900

    ♻️(ch12-6-2): 타입코드 클래스 추가

--- src/main/scala/ch12/ch12-6-2.scala
+++ src/main/scala/ch12/ch12-6-2.scala
@@ -2,23 +2,27 @@ package ch12_6_2
 
 import org.scalatest.Assertions.*
 
-class Employee(val name: String, private var _kind: String) {
+case class EmployeeType(value: String) {
+  override def toString: String = value
+}
+
+class Employee(val name: String, private var _kind: EmployeeType) {
   validateType(_kind)
 
-  def kind: String = _kind
+  def kind: EmployeeType = _kind
 
-  def kind_=(newKind: String): Unit = {
+  def kind_=(newKind: EmployeeType): Unit = {
     validateType(newKind)
     _kind = newKind
   }
 
-  private def validateType(kind: String): Unit = {
-    require(kind == "engineer" || kind == "manager" || kind == "salesperson",
+  private def validateType(kind: EmployeeType): Unit = {
+    require(kind.value == "engineer" || kind.value == "manager" || kind.value == "salesperson",
       s"${kind}라는 직원 유형은 없습니다.")
   }
 
   def capitalizedKind: String = {
-    _kind.capitalize
+    _kind.value.capitalize
   }
 
   override def toString = s"$name ($capitalizedKind)"
@@ -26,15 +30,15 @@ class Employee(val name: String, private var _kind: String) {
 
 // main
 @main def main() = {
-  val engineer = new Employee("Alice", "engineer")
-  val manager = new Employee("Bob", "manager")
-  val salesperson = new Employee("Charlie", "salesperson")
+  val engineer = new Employee("Alice", EmployeeType("engineer"))
+  val manager = new Employee("Bob", EmployeeType("manager"))
+  val salesperson = new Employee("Charlie", EmployeeType("salesperson"))
 
   assertResult("Alice (Engineer)")(engineer.toString)
   assertResult("Bob (Manager)")(manager.toString)
   assertResult("Charlie (Salesperson)")(salesperson.toString)
 
   assertThrows[IllegalArgumentException] {
-    new Employee("Dave", "invalid")
+    new Employee("Dave", EmployeeType("invalid"))
   }
 }
