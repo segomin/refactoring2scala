0a2453551f75de6cb8daa6f54f28a63db8546c93
sangho <segoviamin@gmail.com>
2025-04-06 22:10:51 +0900

    ✨(ch12-11): 슈퍼클래스를 위임으로 바꾸기

---
+++ src/main/scala/ch12/ch12-11.scala
@@ -0,0 +1,33 @@
+package ch12_11
+
+import org.scalatest.Assertions.*
+
+import java.time.OffsetDateTime
+import java.time.temporal.ChronoUnit
+
+
+class CatalogItem(val id: String, val title: String, val tags: List[String]) {
+  def hasTag(tag: String): Boolean = tags.contains(tag)
+}
+
+class Scroll(id: String, title: String, tags: List[String], val lastCleaned: OffsetDateTime) extends CatalogItem(id, title, tags) {
+
+  def needCleaning(targetDate: OffsetDateTime): Boolean = {
+    val threshold = if hasTag("revered") then 700 else 1500
+    daySinceLastCleaning(targetDate) > threshold
+  }
+
+  def daySinceLastCleaning(targetDate: OffsetDateTime): Int = {
+    lastCleaned.until(targetDate, ChronoUnit.DAYS).toInt
+  }
+}
+
+// main
+@main def main() = {
+  val scroll = new Scroll("1", "The Book of Shadows", List("magic", "revered"), OffsetDateTime.now().minusDays(800))
+  assertResult(800)(scroll.daySinceLastCleaning(OffsetDateTime.now()))
+  assertResult(true)(scroll.needCleaning(OffsetDateTime.now()))
+  assertResult(false)(scroll.needCleaning(OffsetDateTime.now().minusDays(600)))
+  assertResult(true)(scroll.hasTag("magic"))
+  assertResult(false)(scroll.hasTag("none"))
+}
