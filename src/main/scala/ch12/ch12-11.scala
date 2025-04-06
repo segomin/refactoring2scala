9e883ff218c1a19cd77dd101d99237f5833312fc
sangho <segoviamin@gmail.com>
2025-04-06 23:50:56 +0900

    ♻️(ch12-11): 고유값을 참조로 바꾸기

--- src/main/scala/ch12/ch12-11.scala
+++ src/main/scala/ch12/ch12-11.scala
@@ -6,12 +6,14 @@ import java.time.OffsetDateTime
 import java.time.temporal.ChronoUnit
 
 
-class Scroll(_id: String, _title: String, _tags: List[String], val lastCleaned: OffsetDateTime) {
-  val _catalogItem = new CatalogItem(_id, _title, _tags)
+class CatalogItem(val id: String, val title: String, val tags: List[String]) {
+  def hasTag(tag: String): Boolean = tags.contains(tag)
+}
 
-  def id: String = _catalogItem.id
-  def title: String = _catalogItem.title
-  def hasTag(tag: String): Boolean = _catalogItem.hasTag(tag)
+class Scroll(_id: String, val catalogItem: CatalogItem, val lastCleaned: OffsetDateTime) {
+  def id: String = catalogItem.id
+  def title: String = catalogItem.title
+  def hasTag(tag: String): Boolean = catalogItem.hasTag(tag)
 
   def needCleaning(targetDate: OffsetDateTime): Boolean = {
     val threshold = if hasTag("revered") then 700 else 1500
@@ -25,7 +27,8 @@ class Scroll(_id: String, _title: String, _tags: List[String], val lastCleaned: 
 
 // main
 @main def main() = {
-  val scroll = new Scroll("1", "The Book of Shadows", List("magic", "revered"), OffsetDateTime.now().minusDays(800))
+  val catalogItem = new CatalogItem("1", "The Book of Shadows", List("magic", "revered"))
+  val scroll = new Scroll("1", catalogItem, OffsetDateTime.now().minusDays(800))
   assertResult(800)(scroll.daySinceLastCleaning(OffsetDateTime.now()))
   assertResult(true)(scroll.needCleaning(OffsetDateTime.now()))
   assertResult(false)(scroll.needCleaning(OffsetDateTime.now().minusDays(600)))
