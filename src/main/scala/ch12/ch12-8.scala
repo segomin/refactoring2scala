5670e2d04786d242b5db79b19a4b2711a3c468d1
sangho <segoviamin@gmail.com>
2025-04-06 18:38:01 +0900

    ♻️(ch12-8): 의도가 같은 함수 선언 바꾸기

--- src/main/scala/ch12/ch12-8.scala
+++ src/main/scala/ch12/ch12-8.scala
@@ -9,9 +9,9 @@ class Employee(name: String, val id: String, val monthlyCost: Int) extends Party
 }
 
 class Department(name: String, val staff: List[Employee]) extends Party(name) {
-  def totalMonthlyCost: Int = staff.map(_.monthlyCost).sum
+  def monthlyCost: Int = staff.map(_.monthlyCost).sum
   def headCount: Int = staff.size
-  def totalAnnualCost: Int = totalMonthlyCost * 12
+  def annualCost: Int = monthlyCost * 12
 }
 
 // main
@@ -20,9 +20,9 @@ class Department(name: String, val staff: List[Employee]) extends Party(name) {
   val employee2 = new Employee("Jane Doe", "E456", 2000)
   val department = new Department("HR", List(employee1, employee2))
 
-  assertResult(3000)(department.totalMonthlyCost)
+  assertResult(3000)(department.monthlyCost)
   assertResult(2)(department.headCount)
-  assertResult(36000)(department.totalAnnualCost)
+  assertResult(36000)(department.annualCost)
 
   // Test the annual cost of an individual employee
   assertResult(12000)(employee1.annualCost)
