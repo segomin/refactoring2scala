f41a49b21bf3d56155e3dfc87afcf3583375e22c
sangho <segoviamin@gmail.com>
2025-04-06 10:43:42 +0900

    ♻️(ch12-6): 서브클래스 추가

--- src/main/scala/ch12/ch12-6.scala
+++ src/main/scala/ch12/ch12-6.scala
@@ -2,11 +2,23 @@ package ch12_6
 
 import org.scalatest.Assertions.*
 
-class Employee(val name: String, val kind: String) {
-  require(kind == "engineer" || kind == "manager" || kind == "salesperson",
-    s"${kind}라는 직원 유형은 없습니다.")
+class Employee(val name: String, val _kind: String) {
+  require(_kind == "engineer" || _kind == "manager" || _kind == "salesperson",
+    s"${_kind}라는 직원 유형은 없습니다.")
+  def kind = _kind
 
   override def toString = s"$name ($kind)"
+}
+
+class Engineer(name: String) extends Employee(name, "engineer") {
+  override val kind: String = "engineer"
+}
+
+def createEmployee(name: String, kind: String): Employee = {
+  kind match {
+    case "engineer" => new Engineer(name)
+    case _ => new Employee(name, kind)
+  }
 }
 
 // main
